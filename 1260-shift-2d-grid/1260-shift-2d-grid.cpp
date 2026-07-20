94e2c0aad05791ed7f354426a258a276a21fee5c
UJJWAL GUPTA <ujjwalgupta2611@gmail.com>
2026-07-20 12:31:35 +0530

    Time: 3 ms (45.51%), Space: 18.2 MB (66.35%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -8,20 +8,11 @@ public:
 
         vector<vector<int>> ans = grid;
 
-        for(int op=1;op<=k;op++) {
-            for(int i=0;i<m;i++) {
-                for(int j=0;j<n;j++) {
-                    if(j+1 < n) ans[i][j+1] = grid[i][j];
-                }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int newIdx = (i * n + j + k) % (m * n);
+                ans[newIdx / n][newIdx % n] = grid[i][j];
             }
-
-            for(int i=0;i<m;i++) {
-                if(i+1 < m) ans[i+1][0] = grid[i][n-1];
-            }
-
-            ans[0][0] = grid[m-1][n-1];
-
-            grid = ans;
         }
 
         return ans;
