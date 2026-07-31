d1a558a21f3b5c26df8a3937dfe27fdbf8a35c02
UJJWAL GUPTA <ujjwalgupta2611@gmail.com>
2026-07-31 15:16:58 +0530

    Time: 0 ms (100%), Space: 12.3 MB (63.89%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
-    bool possible(int papers,vector<int>& citations) {
-        int n = citations.size();
-        int cnt = 0;
-        for(int i=0;i<n;i++){
-            if(citations[i] >= papers) cnt++;
-        }
-        if(cnt >= papers) return true;
-        return false;
-    }
     int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
         int n = citations.size();
-        int h = 0;
-        for(int i=1;i<=n;i++){
-            if(possible(i, citations)) h = i;
 
+        for(int i=0;i<n;i++){
+            if(citations[i] >= n - i) return n-i;
         }
 
-        return h;
-
+        return 0;
     }
 };
