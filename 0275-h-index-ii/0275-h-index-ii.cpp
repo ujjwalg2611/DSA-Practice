76cf09edee6c54f0c09cf51a738065b72d0bd093
UJJWAL GUPTA <ujjwalgupta2611@gmail.com>
2026-07-31 15:46:31 +0530

    Time: 0 ms (100%), Space: 22.7 MB (11.85%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        int l = 0, h = n-1;
+        while(l <= h) {
+            int mid = l + (h - l) / 2;
+            if(citations[mid] >= n - mid) {
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return n - l;
+    }
+};
