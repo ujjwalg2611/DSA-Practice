a9a34d487f9d240c1b5a86b2e2eca1595ff45a95
UJJWAL GUPTA <ujjwalgupta2611@gmail.com>
2026-08-20 11:11:28 +0530

    Time: 3 ms (23.77%), Space: 23.9 MB (66.37%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
@@ -2,30 +2,17 @@ class Solution {
 public:
     vector<int> resultArray(vector<int>& nums) {
         int n = nums.size();
-        stack<int> s1, s2;
-        s1.push(nums[0]);
-        s2.push(nums[1]);
+        vector<int> arr1, arr2;
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
 
         for(int i=2;i<n;i++){
-            if(s1.top() > s2.top()) {
-                s1.push(nums[i]);
-            }else {
-                s2.push(nums[i]);
+            if(arr1[arr1.size() - 1] > arr2[arr2.size() - 1]) {
+                arr1.push_back(nums[i]);
+            } else {
+                arr2.push_back(nums[i]);
             }
         }
-
-        vector<int> arr1, arr2;
-        while(!s1.empty()){
-            arr1.push_back(s1.top());
-            s1.pop();
-        }
-        reverse(arr1.begin(), arr1.end());
-
-        while(!s2.empty()){
-            arr2.push_back(s2.top());
-            s2.pop();
-        }
-        reverse(arr2.begin(), arr2.end());
 
         for(int i=0;i<arr2.size();i++){
             arr1.push_back(arr2[i]);
