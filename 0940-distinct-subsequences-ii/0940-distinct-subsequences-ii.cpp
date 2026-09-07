66209346e3ca3ff37f376b39d783c57aa9fce6c1
UJJWAL GUPTA <ujjwalgupta2611@gmail.com>
2026-09-07 15:14:51 +0530

    Time: 0 ms (100%), Space: 9.7 MB (38.87%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -1,38 +1,22 @@
-class Solution {
-public:
-    const int MOD = 1e9 + 7;
-
-    vector<long long> dp;
-    vector<int> prev;
-
-    long long solve(int i, string &s) {
-        if (i == 0) return 1; 
-
-        if (dp[i] != -1) return dp[i];
-
-        long long ans = (2 * solve(i - 1, s)) % MOD;
-
-        if (prev[i - 1] != -1) {
-            ans = (ans - solve(prev[i - 1], s) + MOD) % MOD;
-        }
-
-        return dp[i] = ans;
-    }
-
-    int distinctSubseqII(string s) {
-        int n = s.size();
-
-        dp.assign(n + 1, -1);
-        prev.assign(n, -1);
-
-        vector<int> last(26, -1);
-
-        for (int i = 0; i < n; i++) {
-            int c = s[i] - 'a';
-            prev[i] = last[c];
-            last[c] = i;
-        }
-
-        return (solve(n, s) - 1 + MOD) % MOD;
-    }
+class Solution { 
+public: 
+    int mod = 1e9 + 7; 
+ 
+    int distinctSubseqII(string s) { 
+        int n = s.size(); 
+        vector<long long> dp(n + 1, 0); 
+        dp[0] = 1; 
+ 
+        vector<long long> last(26, 0); 
+ 
+        for (int i = 1; i <= n; i++) { 
+            int c = s[i - 1] - 'a'; 
+ 
+            dp[i] = (2 * dp[i - 1]) % mod; 
+            dp[i] = (dp[i] - last[c] + mod) % mod; 
+            last[c] = dp[i - 1]; 
+        } 
+ 
+        return (dp[n] - 1 + mod) % mod; 
+    } 
 };
